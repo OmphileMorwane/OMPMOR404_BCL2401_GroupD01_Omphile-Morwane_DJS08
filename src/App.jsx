75f28e659f2598f9e91097bff893bf0a2e1e4b20
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-09 10:24:24 +0200

    Defining an App components to set up routing The BrowserRouter component enables navigation, the header contains links for naviagation. The router component defines which components should ne rendered for each path The route elements specifu the paths and corresponding componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <header>
+      <Link to="/">#VANLIFE</Link>
+      <nav>
+        <Link to="/about">About</Link>
+      </nav>
+    </header>
+    <Routes>
+      <Route path="/" element={Home}/>
+      <Route path="/about" element={About}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
 /**
  * Challenge:
  * Bootstrap the VanLife project by creating the first 2 routes:
@@ -13,12 +36,7 @@
  * Review challenge: do all the CSS yourself based on the design
  * linked in the slides.
  */
-
-function App() {
-
-  return (
-    <h1>Start here</h1>
-  )
-}
-
 export default App
+// ReactDOM
+// .createRoot(document.getElementById('root'))
+// .render(<App/>);
