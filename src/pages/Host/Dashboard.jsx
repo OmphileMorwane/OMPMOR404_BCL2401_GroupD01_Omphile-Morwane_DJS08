27e913653bf67e21aa86d03bace9c1e2bda77565
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-10 15:59:59 +0200

    Added new files in the Host folder

---
+++ src/pages/Host/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Dashboard() {
+    return (
+        <>
+        <h1>Dashboard goes here</h1>
+        <Outlet />
+        </>
+        
+    )
+}
