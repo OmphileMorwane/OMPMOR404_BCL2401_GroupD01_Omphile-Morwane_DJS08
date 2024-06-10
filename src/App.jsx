a724d1bf2be4924f5dd33dc97a0687042a27e619
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-10 08:38:12 +0200

    Installed Mijage

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,9 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from "./pages/Vans"
+import VanDetail from "./pages/VanDetail"
 
-import"./server"
+import "./server"
 
 
 function App() {
