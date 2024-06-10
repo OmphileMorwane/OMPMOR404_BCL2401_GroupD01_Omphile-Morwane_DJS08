2d7f088d8648eb1dcd6612a9f76e69f5b12518e5
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-10 09:53:28 +0200

    Corrected the syntax for routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import About from "./pages/About"
 import Vans from "./pages/Vans"
 import VanDetail from "./pages/VanDetail"
 
-import "./server"
-
-
 function App() {
   return (
     <BrowserRouter>
@@ -19,9 +16,10 @@ function App() {
       </nav>
     </header>
     <Routes>
-      <Route path="/" element={Home}/>
-      <Route path="/about" element={About}/>
-      <Route path="/vans" element={Vans}/>
+      <Route path="/" element={<Home />}/>
+      <Route path="/About" element={< About />}/>
+      <Route path="/Vans" element={ <Vans />}/>
+      <Route path="/vans/id" element={<VanDetail />}/>
     </Routes>
     </BrowserRouter>
   )
