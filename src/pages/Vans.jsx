6f4160c990ad445f32e62a3dba695941f30ee460
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-10 11:18:33 +0200

    wrapped van-tile contents in a link

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
+import server from '../../server'
+server(); // Server.js was not rendering, called an empty function to render it.
+import { Link } from "react-router-dom"
 
 export default function Vans() {
   const [vans, setVans] = React.useState([])
   React.useEffect(() => {
     fetch("/api/vans")
     .then(res => res.json())
-    .then(data => conaole.log(data))
+    .then(data => setVans(data.vans))
   }, [])
 
  const vanElements = vans.map(van => (
   <div key={van.id} className="van-tile">
-    <img src={van.imageUrl} />
-    <div className="van-info">
-      <h3>{van.name}</h3>
-      <p>${van.price}<span>/day</span></p>
-    </div>
-      <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    <Link to={`/vans/${van.id}`}>
+        <img src={van.imageUrl} />
+        <div className="van-info">
+          <h3>{van.name}</h3>
+          <p>${van.price}<span>/day</span></p>
+        </div>
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    </Link>
   </div>
  ))
   
